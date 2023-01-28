c7784f2b725c0e26dccdc9781bea115b88add486
HyunJoo Kim <hyunjoogo@gmail.com>
2023-01-29 02:31:56 +0900

    Update App.tsx

--- src/components/app/App.tsx
+++ src/components/app/App.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import { Container } from "react-bootstrap";
 import Inputs from "./Inputs";
 import Buttons from "./Buttons";
@@ -16,9 +16,9 @@ function App() {
   return (
     <Container>
       <h1 className="text-center py-2">MS-REWORD-MACRO</h1>
-      <IsEdge />
+      <IsEdge/>
       <div className="app">
-        <HowToUse />
+        <HowToUse/>
         <Inputs
           status={status}
           prefix={prefix}
